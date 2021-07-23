d8bf80eac431f4585ca87436a89854ec881f122a
Ivan Laver <ereyzer142@gmail.com>
2021-07-23 17:13:52 +0300

    notify

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,39 +3,53 @@ import React from "react";
 import { ContactForm } from "../ContactForm/ContactForm";
 import { ContactList } from "../ContactList/ContactList";
 import { Filter } from "../Filter/Filter";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export class App extends React.Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
     filter: "",
   };
 
-  handleSubmit = (data) => {
+  componentDidMount() {
+    localStorage.getItem("list") &&
+      this.setState({ contacts: JSON.parse(localStorage.getItem("list")) });
+  }
+
+  addLocalStorage = () =>
+    localStorage.setItem("list", JSON.stringify(this.state.contacts));
+
+  handleSubmit = async (data) => {
     const test = this.state.contacts.some(({ name }) => name === data.name);
     if (test) {
-      alert(data.name + " is already exist");
+      toast(data.name + " is already exist");
+      // alert(data.name + " is already exist");
       return;
     }
     // this.setState({
     //   filter: "",
 
     // })
-    this.setState((prevState) => {
+    await this.setState((prevState) => {
       return {
         contacts: [...prevState.contacts, data],
         filter: "",
       };
     });
-    alert("you have new contact");
+    toast("you have new contact");
+    // alert("you have new contact");
+    await this.addLocalStorage();
   };
 
   findInFilter = () => {
     const { contacts, filter } = this.state;
+
     if (filter === "") return contacts;
 
     const testing = contacts.filter(({ name }) => this.testFunc(filter, name));
@@ -63,8 +77,8 @@ export class App extends React.Component {
     return filter.toLowerCase() === test.toLowerCase() ? true : false;
   }
 
-  deleteContact = (delId) => {
-    this.setState(({ contacts }) => {
+  deleteContact = async (delId) => {
+    await this.setState(({ contacts }) => {
       return {
         contacts: contacts.reduce(
           (acc, contact) =>
@@ -73,6 +87,7 @@ export class App extends React.Component {
         ),
       };
     });
+    await this.addLocalStorage();
   };
 
   render() {
@@ -85,6 +100,18 @@ export class App extends React.Component {
         <h2>Contacts</h2>
         <Filter handleChange={this.handleChange} filter={this.state.filter} />
         <ContactList contacts={renderArr} deleteContact={this.deleteContact} />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <ToastContainer />
       </div>
     );
   }
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import style from "./ContactForm.module.css";
 import { v4 as uuidv4 } from "uuid";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export class ContactForm extends Component {
   static propTypes = {
@@ -28,7 +30,8 @@ export class ContactForm extends Component {
     const testName = name.trim();
     const testNumber = number.trim();
     if (testName === "" || testNumber === "") {
-      alert("fill in all fields");
+      toast("fill in all fields");
+      // alert("fill in all fields");
       return;
     }
 
@@ -42,48 +45,47 @@ export class ContactForm extends Component {
   render() {
     const { inputIdName, inputIdNumber, state, submitForm } = this;
     return (
-      <form className={style.Form} onSubmit={submitForm}>
-        <label htmlFor={inputIdName} className={style.Label}>
-          Name
-        </label>
-        <input
-          id={inputIdName}
-          className={style.Input}
-          type="text"
-          name="name"
-          value={state.name}
-          onChange={this.handleChange}
-        ></input>
-        <label htmlFor={inputIdNumber} className={style.Label}>
-          Number
-        </label>
-        <input
-          id={inputIdNumber}
-          className={style.Input}
-          type="tel"
-          name="number"
-          value={state.number}
-          onChange={this.handleChange}
-        ></input>
-
-        {/* <Input 
-            className={style.Item}
+      <>
+        <form className={style.Form} onSubmit={submitForm}>
+          <label htmlFor={inputIdName} className={style.Label}>
+            Name
+          </label>
+          <input
+            id={inputIdName}
+            className={style.Input}
+            type="text"
             name="name"
-            label="Name"
-            sendValue={this.sendValue}
-            
-            />
-            <Input 
-            className={style.Item}
-            name="namber"
-            label="Number"
-            sendValue={this.sendValue}
-            
-            /> */}
-        <button type="submit" className={style.AddBtn}>
-          Add contact
-        </button>
-      </form>
+            value={state.name}
+            onChange={this.handleChange}
+          ></input>
+          <label htmlFor={inputIdNumber} className={style.Label}>
+            Number
+          </label>
+          <input
+            id={inputIdNumber}
+            className={style.Input}
+            type="tel"
+            name="number"
+            value={state.number}
+            onChange={this.handleChange}
+          ></input>
+          <button type="submit" className={style.AddBtn}>
+            Add contact
+          </button>
+        </form>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <ToastContainer />
+      </>
     );
   }
 }
