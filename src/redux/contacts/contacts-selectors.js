9e75855044da7502d553930e96c5e4d934527a96
Ivan Laver <ereyzer142@gmail.com>
2021-09-07 14:47:26 +0300

    add memoize

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,7 +1,15 @@
+import { createSelector } from 'reselect';
 import { filterItems } from '../../helpers/filterItems';
 export const getContacts = ({ contacts }) => contacts.items;
 export const getFilter = ({ contacts }) => contacts.filter;
-export const getContactsWithFilter = data =>
-  filterItems(getContacts(data), getFilter(data));
 export const isLoading = ({ contacts }) => contacts.loading;
 export const error = ({ contacts }) => contacts.error;
+// export const getContactsWithFilter = data =>
+//   filterItems(getContacts(data), getFilter(data));
+
+export const getContactsWithFilter = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) => {
+    return filterItems(contacts, filter);
+  },
+);
